1e3c4c80e62278233433f5dbb750f923e9ecad7b
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2015-09-19 09:22:03 +0300

    Add movie booking example

--- app/pages/frontPage.js
+++ app/pages/frontPage.js
@@ -1,25 +1,127 @@
 import React from 'react'
 import Bacon from 'baconjs'
+import inBrowser from './inBrowser'
+import request from 'superagent-bluebird-promise'
+
+const showMovies = applicationState =>
+    <ul className="movies">
+    {
+        applicationState.movies.map((movie, key) =>
+            <li className="movies__movie" key={key}>
+                <div className="movies__movie__image-container">
+                    <img src={movie.imageUrl}/>
+                </div>
+                <div className="movies__movie__details">
+                    <h2>{movie.title}</h2>
+                    <h3>{movie.startTime}</h3>
+                    <input
+                        type="number"
+                        placeholder="Number of tickets"
+                        value={applicationState.bookings[movie.id] || undefined}
+                        onChange={evt => amountOfTicketsInput.push({movieId: movie.id, textInput: evt.target.value}) }
+                        />
+                    <button
+                        onClick={() => bookingButtonClickedBus.push()}
+                        disabled={!applicationState.bookings[movie.id]}>
+                        Book now
+                    </button>
+                </div>
+            </li>
+        )
+    }
+    </ul>
+
+const showBookings = applicationState =>
+    <div>
+        <h2>Your bookings</h2>
+        <ul>
+        {
+            applicationState
+                .movies
+                .filter(({id}) => Object.keys(applicationState.bookings).indexOf(id) > -1)
+                .map((bookedMovie, key) =>
+                    <li key={key}>
+                        <h3>{bookedMovie.title}, {bookedMovie.startTime}</h3>
+                        <div>{applicationState.bookings[bookedMovie.id]} tickets</div>
+                    </li>
+                )
+        }
+        </ul>
+    </div>
 
 export const renderPage = applicationState =>
     <body>
-        <h1>hello unistack!</h1>
-        <button onClick={() => buttonClickedBus.push()}>
-            click me
-        </button>
-        <div>{applicationState.clicks}</div>
+        <h1 className="page-title">{pageTitle}</h1>
+        {(() => {
+            switch (true) {
+                case '/' == applicationState.url:
+                    return showMovies(applicationState)
+                case new RegExp('/user/.*/bookings').test(applicationState.url):
+                    return showBookings(applicationState)
+                default:
+                    return `Could not find a route for ${applicationState.url}`
+            }
+        })()}
     </body>
 
-export const initialState = {
-    clicks: 0
+export const pagePath = new RegExp("((^\/$|^\/user/.*))")
+
+export const findUserId = url => {
+    const userIdFromUrl = url.match("/user/(.*?)/")
+    return userIdFromUrl ? userIdFromUrl[1] : undefined
 }
 
-export const pagePath = '/'
+export const initialState = (movies, url, bookings = {}) => ({
+    movies,
+    url,
+    bookings
+})
 
-export const pageTitle = 'Unistack'
+export const pageTitle = 'Book movie tickets'
 
-const buttonClickedBus = new Bacon.Bus()
+const userId = () => {
+    const localStorageKey = 'bookingAppUserId'
+    const storedUserId = localStorage.getItem(localStorageKey)
+    if (storedUserId) {
+        return storedUserId
+    } else {
+        const generatedUserId = Math.random().toString()
+        localStorage.setItem(localStorageKey, generatedUserId)
+        return generatedUserId
+    }
+}
+
+const bookingButtonClickedBus = new Bacon.Bus()
+const urlStream = Bacon.mergeAll(
+    bookingButtonClickedBus.map(() => `/user/${userId()}/bookings`).doAction(url => history.pushState({}, '', url)),
+    inBrowser ?
+        Bacon
+            .fromBinder(sink => { window.onpopstate = sink })
+            .map(() => document.location.pathname)
+        :
+        Bacon.never()
+)
+
+const amountOfTicketsInput = new Bacon.Bus()
+const bookingsStream = amountOfTicketsInput
+    .filter(({textInput}) => /\d+/.test(textInput) || textInput === "")
+    .map(({movieId, textInput}) => ({
+        movieId, amountOfTickets: textInput.length == 0 ? 0 : parseInt(textInput)
+    }))
+    .flatMap(({movieId, amountOfTickets}) =>
+        Bacon
+            .fromPromise(request.put(
+                `/api/users/${userId()}/bookings/${movieId}?amountOfTickets=${amountOfTickets}`
+            ))
+            .map(({movieId, amountOfTickets}))
+    )
+
 export const applicationStateProperty = initialState => Bacon.update(
     initialState,
-    buttonClickedBus, applicationState => ({...applicationState, clicks: applicationState.clicks + 1})
+    bookingsStream, (applicationState, {movieId, amountOfTickets}) => {
+        const previousBookings = applicationState.bookings
+        const nextBookings = {...previousBookings, [movieId]: amountOfTickets}
+        return { ...applicationState, bookings: nextBookings}
+    },
+    urlStream, (applicationState, url) => ({...applicationState, url})
 ).doLog('application state')
--- app/server.js
+++ app/server.js
@@ -17,6 +17,29 @@ server.use(compression({threshold: 512}))
 const cssFilePath = path.resolve(`${__dirname}/../.generated/style.css`)
 const bundleJsFilePath = path.resolve(`${__dirname}/../.generated/bundle.js`)
 
+const movies = [ // An in-memory movie database
+    {
+        id: '3ad9aa',
+        title: 'Streets of London',
+        imageUrl: '/images/streets-of-london.jpg',
+        startTime: '6:30 PM'
+    },
+    {
+        id: 'f4f5a6',
+        title: 'Rafters',
+        imageUrl: '/images/rafters.jpg',
+        startTime: '8 PM'
+    },
+    {
+        id: '030103',
+        title: 'The Banana Agent',
+        imageUrl: '/images/banana-agent.jpg',
+        startTime: '9:30 PM'
+    }
+]
+
+var bookings = {} // An in-memory bookings database
+
 server.get('*', (req, res, next) => {
     const page = pages.findPage(req.url)
     if (page) {
@@ -25,7 +48,7 @@ server.get('*', (req, res, next) => {
             .then(([cssChecksum, bundleJsChecksum]) => {
                 res.send(ReactDOMServer.renderToString(basePage(
                     page,
-                    page.initialState,
+                    page.initialState(movies, req.url, bookings[page.findUserId(req.url)]),
                     { cssChecksum, bundleJsChecksum}
                 )))
             })
@@ -33,6 +56,15 @@ server.get('*', (req, res, next) => {
     } else {
         next()
     }
+})
+
+server.put('/api/users/:userId/bookings/:movieId', (req, res, next) => {
+    var userId = req.params.userId
+    const userBookings = bookings[userId] || {}
+    bookings[userId] = {...userBookings,
+        [req.params.movieId]: parseInt(req.query.amountOfTickets)
+    }
+    res.send('ok')
 })
 
 const serveStaticResource = filePath => (req, res, next) => {
@@ -52,6 +84,8 @@ const serveStaticResource = filePath => (req, res, next) => {
 server.get('/style.css', serveStaticResource(cssFilePath))
 
 server.get('/bundle.js', serveStaticResource(bundleJsFilePath))
+
+server.use('/images', express.static(`${__dirname}/images`))
 
 const checksumPromise = filePath =>
     fs
